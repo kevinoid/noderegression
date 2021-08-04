kevinoid/noderegression

commit d002fb158f173a851c90d90fe6ea371079e2cb25

Do not modify build objects
Since they may be provided by the caller.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>

lib/get-build-list.js
@@ -76,14 +76,7 @@ function filterAndNormalizeBuilds(builds) {
       return false;
     }
 
-    const {
-      minor,
-      patch,
-      year,
-      month,
-      day,
-      commit,
-    } = parseBuildVersion(build.version);
+    const { minor, patch, commit } = parseBuildVersion(build.version);
 
     // Commits which are not ancestors of master are not helpful for bisecting
     // a regression on master.
@@ -110,12 +103,6 @@ function filterAndNormalizeBuilds(builds) {
       return false;
     }
     commitToVersion.set(commit, build.version);
-
-    // Add commit to build for use during bisection
-    build.commit = commit;
-
-    // Use date from version, which seems to be more reliable for ordering
-    build.date = `${year}-${month}-${day}`;
 
     return true;
   });

commit 0d39143eb1e38ac54d686baafa14a83ff9ee1350

fix: parse version in reorderRange()
.commit is no longer added to build objects during normalization, so it
must be parsed from the version string as required.

Signed-off-by: Kevin Locke <kevin@kevinlocke.name>
Fixes: d002fb158f17 ("Do not modify build objects")

lib/get-build-list.js
@@ -117,10 +117,11 @@ function filterAndNormalizeBuilds(builds) {
  */
 function reorderRange(builds, start, end, commitOrder) {
   for (const build of builds.slice(start, end)) {
-    const pos = commitOrder.indexOf(build.commit);
+    const { commit } = parseBuildVersion(build.version);
+    const pos = commitOrder.indexOf(commit);
     if (pos < 0) {
       throw new Error(
-        `commit ${build.commit} not found in ordering for ${build.date}`,
+        `commit ${commit} not found in ordering for ${build.date}`,
       );
     }
 